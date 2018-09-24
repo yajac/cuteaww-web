e08ff0628080a4f3b8f19987700aed5ff197265e
yajac <yajac.com@gmail.com>
2018-09-23 21:26:01 -0400

    Update to the new webserivce

--- webpage/js/cuteaww.js
+++ webpage/js/cuteaww.js
@@ -1,41 +1,53 @@
-var urlBase = "https://fmwrsonnjg.execute-api.us-east-1.amazonaws.com/Stage/"
+var urlBase = "https://glrecdzo5k.execute-api.us-east-1.amazonaws.com/Dev/"
 
-var jsPromiseAww = Promise.resolve(
-  $.ajax({
-    type: "GET",
-    url: urlBase + "aww",
-    dataType: "json",
-    crossDomain: true
-  })
-);
+var subreddits = ['catpictures','cats','Catswithjobs','dogpictures','dogswithjobs','dogs','aww','awwgifs','animalgifs'];
 
 Vue.component('results-list', {
     data: function () {
         return {
             value: 0,
             posts: [],
-            selected: ['catpictures','dogpictures','aww'],
+            selected: subreddits,
             optionsCat: [
-              {text: '/r/catpictures', value: 'catpictures'}
+              {text: '/r/cats', value: 'cats'},
+              {text: '/r/catpictures', value: 'catpictures'},
+              {text: '/r/catswithjobs', value: 'Catswithjobs'}
             ],
             optionsDog: [
-              {text: '/r/dogpictures', value: 'dogpictures'}
+              {text: '/r/dogs', value: 'dogs'},
+              {text: '/r/dogpictures', value: 'dogpictures'},
+              {text: '/r/dogswithjobs', value: 'dogswithjobs'}
             ]
             ,
             optionsAnimals: [
-              {text: '/r/aww', value: 'aww'}
+              {text: '/r/aww', value: 'aww'},
+              {text: '/r/awwgifs', value: 'awwgifs'},
+              {text: '/r/animalgifs', value: 'animalgifs'}
             ]
         }
     },
     created(){
-      jsPromiseAww.then((response) => {
-          for(var index in response){
-            var post = response[index];
-            if(post.bandwidth  > 1 && !post.is_album && !post.link.endsWith("mp4")){
-              this.posts =  this.posts.concat(post);
-            }
-          }
-      });
+
+      for(var subreddit in subreddits){
+          var promise = Promise.resolve(
+            $.ajax({
+              type: "GET",
+              url: urlBase + subreddits[subreddit],
+              dataType: "json",
+              crossDomain: true
+            })
+          )
+          promise.then(response => {
+              for(var index in response){
+                var post = response[index];
+                if(post.bandwidth  > 1 && !post.is_album && !post.link.endsWith("mp4")){
+                  this.posts =  this.posts.concat(post);
+                }
+              }
+          });
+
+
+        }
 
     },
     methods: {
@@ -61,6 +73,10 @@ Vue.filter('formatPostURL', function(url) {
     return url.substring(0, url.length - 5) + ".gif";
   }
   return url;
+});
+
+Vue.filter('formatImgurURL', function(id) {
+  return 'https://imgur.com/' + id;
 });
 
 new Vue({
